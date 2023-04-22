e1062348bcbeb359464d4b515dbc051afe59e90f
qmeisman <qmeisman@gmail.com>
2023-04-22 14:10:28 -0600

    This is a potential main

--- script.py
+++ script.py
@@ -28,6 +28,40 @@ def post_req(url, payload):
     finally: return res
 
 def main():
+    alexa = Helper(name='Alexa')
+
+    while True:
+        upc_code = scan_upc_code()
+        res = get_req(BASE_URL + '/product/upc/' + upc_code)
+        #if the item is not in data base
+        if res.status_code < 200 or res.status_code > 299:
+            alexa.say_this("This item is not reckognized. Would you like to add this item to the database?", feedback=True)
+            answer = alexa.listen_for_command(feedback=True)
+            if answer.startswith('yes') or answer.startswith('yeah'):
+                while True:
+                    alexa.say_this("What is the name of the product you would like to add?", feedback=True)
+                    item_to_add = alexa.listen_for_command(feedback=True)
+                    alexa.say_this("The item you would like to add is " + item_to_add + "is that correct?", feedback=True)
+                    answer = alexa.listen_for_command(feedback=True)
+                    if answer.startswith('yes') or answer.startswith('yeah'):
+                        break
+                payload = {'name': item_to_add, 'upcCode': upc_code}
+                posted = post_req(BASE_URL + '/product', payload)
+                if res == None: return
+
+                if res.status_code < 200 or res.status_code > 299:
+                    print('Post request failed with status code: ', res.status_code, '\n', 'message: ', res.text)
+                    return
+        else:
+            #print/speak the name of the object
+            alexa.say_this("Your item is " + res.json()[0].name, feedback=True)
+        alexa.say_this("Would you like to scan another item?", feedback=True)
+        answer = alexa.listen_for_command(feedback=True)
+        if answer.startswith('no'):
+            break
+
+'''
+def main():
     #get request example
     res = get_req(BASE_URL + '/product/upc/' + scan_upc_code())
 
@@ -52,6 +86,7 @@ def main():
     
     print(res.json())
 
+'''
 
 if __name__ == '__main__':
     main()
